ef4951942d70c83e9c79ded8c8b6f70fa31c3f41
Julien Monat Rodier <julien.monat.rodier@gmail.com>
2012-05-21 14:49:06 -0400

    - added recrutable and recruter

---
+++ src/2dgame/2dgame/Components/Gameplay/Recruter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Barebones.Components;
+
+namespace _2dgame.Components.Gameplay
+{
+    class Recruter : EntityComponent
+    {
+        List<Recrutable> m_Followers = new List<Recrutable>();
+
+        public override IEnumerable<Barebones.Dependencies.IDependency> GetDependencies()
+        {
+            yield break;
+        }
+
+        public void AddFollower(Recrutable recrute)
+        {
+            m_Followers.Add(recrute);
+        }
+    }
+}
